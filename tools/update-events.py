66b4d0f8766b2d3a51091ab8de6510c9bf3bd00b
Matthew Leeds <mwl458@gmail.com>
2015-08-24 13:01:47 -0500

    Fix API URL signature.

--- tools/update-events.py
+++ tools/update-events.py
@@ -15,7 +15,7 @@ import sys
 
 num_events = 5
 
-api_call = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Code-for-Tuscaloosa&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=142197972&sig=30469a8bbe49fb53a9dacdfe8f9f6485a047a9d2a'
+api_call = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Code-for-Tuscaloosa&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=142197972&sig=30469a8bbe49fb53a9dacdfe8f9f6485a047a9d2'
 script_path = os.path.dirname(os.path.realpath(__file__))
 include_path = os.path.dirname(script_path) + '/_includes/upcoming.html'
 
@@ -49,5 +49,5 @@ try:
     print('Ready to commit!')
 
 except:
-    print(sys.exc_info()[0], file=sys.stderr)
+    print(sys.exc_info(), file=sys.stderr)
     sys.exit(1)
